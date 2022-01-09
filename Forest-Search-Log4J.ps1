983d14b704a829fe720316e7e2b38713baf92047
Sailingbikeruk <29166333+sailingbikeruk@users.noreply.github.com>
2022-01-09 14:21:45 +0000

    Initial Commit
    Variant to search files on all servers, in all domains of a multi domain forest

---
+++ Forest-Search-Log4J.ps1
@@ -0,0 +1,26 @@
+$OutputFilePath = "c:\temp\log4j.csv" # change this to satisfy your needs. The path must exist.
+
+# Get Windows Server from Active Directory - you can amend this to gather all computers or create your own filter. I needed to be an Enterprise Admin.
+# I sorted it whilst testing as I had put test files on d: and e: for some servers and it meant these came up first. It is not necessray in operation.
+
+$Domains = (Get-Adforest).domains
+foreach($Domain in $Domains){
+  $Computers = get-adcomputer -filter 'OperatingSystem -like "*Windows Server*"' -server $Domain | select DNSHostName | sort -property DNSHostName
+  }
+
+# Invoke this command on all the computers listed in $computers. 
+# It will send the command to the first 32 computers in the variable as long as they can be reached an PS rmeoting is enabled
+# Write-Host is used to write update information to the console and NOT to the pipeline so that the detail isn't inlcuded in the export.
+
+invoke-command -ComputerName $computers.dnshostname -ErrorAction silentlycontinue -ScriptBlock {
+  Write-Host "`nChecking $($env:ComputerName)" -nonewline # Tell the users what computer is being tested. Useful to see progress
+  $volumes = $null #sets the variable to $null
+  $volumes = Get-Volume | # gets all the volumes on the target computer
+  where -FilterScript {($_.Driveletter -ne "`0") -and ($_.DriveType -eq "Fixed")} # removes non-fixed drives and drives with no letter associated such as system partitions
+  foreach ($Volume in $Volumes) {
+    write-Host "." -nonewline # This just confrms to the user that the script hasn't frozen when testing larger drives. 
+    get-childitem ($Volume.Driveletter + ":\") -rec -force -filter *.jar -ea silentlycontinue | 
+    select-string "JndiLookup.class" | 
+    select Path 
+    }
+} | export-csv $OutputFilePath
